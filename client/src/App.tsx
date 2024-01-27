9b8345bbee75bdd33b05ed27e6eea57ad7da7045
Shigi-p <shigi.522@gmail.com>
2024-01-27 10:38:46 +0900

    update(rust): add rust files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import "./App.scss";
+import init, { add } from "./pkg";
 
 function App() {
+  const a = 1;
+  const b = 2;
+
+  useEffect(() => {
+    init().then(() => {
+      console.log(`a is ${a}`);
+      console.log(`b is ${b}`);
+      console.log(`a + b is ${add(1, 1)}`);
+    });
+  }, []);
+
   return (
     <>
       <h1>Comparing setTimeout javascript and wasm</h1>
